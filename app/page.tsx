e216a2c6f78d93cb2d65c7b1dd1c8097019a59a1
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-25 22:50:02 -0300

    V1.2: Bigger mobile texts

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
 
   return (
 		<main className='w-3/4 mx-auto sm:ml-auto sm:mr-0'>
-			<h1 className='mt-24 text-center font-bold text-2xl sm:text-4xl sm:text-left mb-10'>GI WORDLE</h1>
+			<h1 className='mt-24 text-black text-center font-bold text-2xl sm:text-4xl sm:text-left mb-10'>GI WORDLE</h1>
 			<div className='flex flex-col gap-3 w-auto h-dvh justify-start'>
 				{words.map((word: string, index: number)=> (
 					<Word word={word} key={index} />

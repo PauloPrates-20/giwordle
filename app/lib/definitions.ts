9dde17cb49d9259d04da7ec8c2732193f79e5d77
Paulo Prates <paulo.prates01@hotmail.com>
2024-10-28 10:53:12 -0300

    feat: autonav letters

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -3,6 +3,18 @@ export type LetterState = 'wrong' | 'misplaced' | 'correct' | undefined
 export type LetterProps = {
 	letter: string;
 	letterState: LetterState;
-	position: number;
+	letterIndex: number;
+	wordIndex: number;
+	active: boolean;
 	handleChange: (value: string, position: number) => void;
+	setActiveLetter: (index: number) => void;
+	setActiveWord: (index: number) => void;
+}
+
+export type WordProps = {
+	word: string;
+	wordCount: number;
+	wordIndex: number;
+	active: boolean;
+	setActive: (index: number) => void;
 }
